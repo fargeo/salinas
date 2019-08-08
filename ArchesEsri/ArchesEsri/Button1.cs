a672ee6a10350b9bfaab5af2da0cbfddbcb11f9f
chiatt <chiatt@fargeo.com>
2019-08-08 12:04:51 -0700

    Adds ajax request to qa.archesproject.org search data

--- ArchesEsri/ArchesEsri/Button1.cs
+++ ArchesEsri/ArchesEsri/Button1.cs
@@ -1,29 +1,38 @@
-﻿using System;
+﻿using ArcGIS.Desktop.Framework.Contracts;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Net;
+using System.Net.Http;
 using System.Threading.Tasks;
-using ArcGIS.Core.CIM;
-using ArcGIS.Core.Data;
-using ArcGIS.Core.Geometry;
-using ArcGIS.Desktop.Catalog;
-using ArcGIS.Desktop.Core;
-using ArcGIS.Desktop.Editing;
-using ArcGIS.Desktop.Extensions;
-using ArcGIS.Desktop.Framework;
-using ArcGIS.Desktop.Framework.Contracts;
-using ArcGIS.Desktop.Framework.Dialogs;
-using ArcGIS.Desktop.Framework.Threading.Tasks;
-using ArcGIS.Desktop.Mapping;
 
 namespace ArchesEsri
 {
     internal class Button1 : Button
     {
+
+
+        // HttpClient is intended to be instantiated once per application, rather than per-use. See Remarks.
+        static readonly HttpClient client = new HttpClient();
+
+        static async Task Taco()
+        {
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync("http://qa.archesproject.org/search/resources");
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+
+                ArcGIS.Desktop.Framework.Dialogs.MessageBox.Show($"Project path: {responseBody}", "Project Info");
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine("\nException Caught!");
+                Console.WriteLine("Message :{0} ", e.Message);
+            }
+        }
         protected override void OnClick()
         {
-            string stringURI = ArcGIS.Desktop.Core.Project.Current.URI;
-            ArcGIS.Desktop.Framework.Dialogs.MessageBox.Show($"Project path:  {stringURI}", "Project Info");
+            Taco();
         }
     }
 }
