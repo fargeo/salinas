3b5949e95a4c1b95f7f3bf592e26cb20bfa5e912
njkim <nkim@fargeo.com>
2019-08-10 08:05:51 -0700

    Create EditWithInspector

---
+++ EditWithInspectro/EditWithInspectro/ModValueToSetEditBox.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EditWithInspectro
+{
+    class ModValueToSetEditBox : ArcGIS.Desktop.Framework.Contracts.EditBox
+    {
+        public ModValueToSetEditBox()
+        {
+            Module1.Current.ModValueToSetEditBox1 = this;
+            Text = "";
+        }
+    }
+}
