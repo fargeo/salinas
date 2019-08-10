738d2cd5b9359ff6a9074461a8a9eef330b9a65b
njkim <nkim@fargeo.com>
2019-08-10 08:04:22 -0700

    create ArchesPane Project

---
+++ ArchesPane/ArchesPane/Dockpane1.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ArchesPane
+{
+    /// <summary>
+    /// Interaction logic for Dockpane1View.xaml
+    /// </summary>
+    public partial class Dockpane1View : UserControl
+    {
+        public Dockpane1View()
+        {
+            InitializeComponent();
+
+            List<ArchesAttribute> items = new List<ArchesAttribute>
+            {
+                new ArchesAttribute() { Name = "attr1", Desc = "Attribute #1" },
+                new ArchesAttribute() { Name = "attr2", Desc = "Attribute #2" },
+                new ArchesAttribute() { Name = "attr3", Desc = "Attribute #3" }
+            };
+            ArchesAttributeList.ItemsSource = items;
+        }
+    }
+
+    public class ArchesAttribute
+    {
+        public string Name { get; set; }
+        public string Desc { get; set; }
+    }
+}
---
+++ ArchesPane/Source.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows;
+using System.Collections.Generic;
+
+namespace ArchesPane.Dockpane1View
+{
+    public partial class ItemsControlDataBinding : Window
+    {
+        public ItemsControlDataBinding()
+        {
+            InitializeComponent();
+
+            List<ArchesAttribute> items = new List<ArchesAttribute>();
+            items.Add(new ArchesAttribute() {Name = "attr1", Desc = "Attribute #1" });
+            items.Add(new ArchesAttribute() {Name = "attr2", Desc = "Attribute #2" });
+            items.Add(new ArchesAttribute() {Name = "attr3", Desc = "Attribute #3" });
+
+            ArchesAttributeList.ItemsSource = items;
+        }
+    }
+
+    public class ArchesAttribute
+    {
+        public string Name { get; set; }
+        public string Desc { get; set; }
+    }
+}
